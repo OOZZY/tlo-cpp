f7bac9334f07880520460b3c6d5512f5c4269478
oozzy <oozzy.tlo@gmail.com>
2020-02-23 19:53:29 -0800

    Add comments containing citations

--- src/hash.cpp
+++ src/hash.cpp
@@ -1,6 +1,9 @@
 #include "tlo-cpp/hash.hpp"
 
 namespace tlo {
+// Hash combining algorithm from web page "List (Java Platform SE 8)" available
+// at: https://docs.oracle.com/javase/8/docs/api/java/util/List.html#hashCode--
+// (Retrieved February 11, 2020)
 JavaStyleHashCombiner &JavaStyleHashCombiner::combineWith(
     std::size_t nextHash) {
   hash = 31 * hash + nextHash;
@@ -9,6 +12,10 @@ JavaStyleHashCombiner &JavaStyleHashCombiner::combineWith(
 
 std::size_t JavaStyleHashCombiner::getHash() { return hash; }
 
+// Hash combining algorithm from web page "Header <boost/functional/hash.hpp>"
+// available at:
+// https://www.boost.org/doc/libs/1_55_0/doc/html/hash/reference.html#boost.hash_combine
+// (Retrieved February 11, 2020)
 BoostStyleHashCombiner &BoostStyleHashCombiner::combineWith(
     std::size_t nextHash) {
   hash ^= nextHash + 0x9e3779b9 + (hash << 6) + (hash >> 2);
