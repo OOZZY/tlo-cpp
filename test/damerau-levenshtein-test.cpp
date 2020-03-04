778b51a4aa8449cbb4ac3298f3192601b7233562
oozzy <oozzy.tlo@gmail.com>
2020-03-03 18:54:07 -0800

    Use TLO_EXPECT_EQ() in edit distance tests

--- test/damerau-levenshtein-test.cpp
+++ test/damerau-levenshtein-test.cpp
@@ -6,40 +6,42 @@ namespace {
 using namespace std::string_literals;
 
 TLO_TEST(damerLevenDistance1) {
-  TLO_EXPECT(tlo::damerLevenDistance1(""s, ""s) == 0U);
-  TLO_EXPECT(tlo::damerLevenDistance1("GAC"s, ""s) == 3U);
-  TLO_EXPECT(tlo::damerLevenDistance1(""s, "AGCAT"s) == 5U);
-  TLO_EXPECT(tlo::damerLevenDistance1("GAC"s, "AGCAT"s) == 3U);
-  TLO_EXPECT(tlo::damerLevenDistance1("XMJYAUZ"s, "MZJAWXU"s) == 6U);
-  TLO_EXPECT(tlo::damerLevenDistance1("___XMJYAUZ___"s, "__MZJAWXU___"s) == 7U);
-  TLO_EXPECT(tlo::damerLevenDistance1("__XMJYAUZ___"s, "___MZJAWXU___-"s) ==
-             7U);
-  TLO_EXPECT(tlo::damerLevenDistance1("0123456789"s, "0123456789"s) == 0U);
-  TLO_EXPECT(tlo::damerLevenDistance1("01234567"s, "abcdefghij"s) == 10U);
-  TLO_EXPECT(tlo::damerLevenDistance1("aaabbb"s, "aaabbb___bbbccc"s) == 9U);
-  TLO_EXPECT(tlo::damerLevenDistance1("bbbccc"s, "aaabbb___bbbccc"s) == 9U);
-  TLO_EXPECT(tlo::damerLevenDistance1("aaabbbccc"s, "aaabbb___bbbccc"s) == 6U);
-  TLO_EXPECT(tlo::damerLevenDistance1("sitting"s, "kitten"s) == 3U);
-  TLO_EXPECT(tlo::damerLevenDistance1("Sunday"s, "Saturday"s) == 3U);
-  TLO_EXPECT(tlo::damerLevenDistance1("CA"s, "ABC"s) == 2U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance1(""s, ""s), 0U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance1("GAC"s, ""s), 3U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance1(""s, "AGCAT"s), 5U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance1("GAC"s, "AGCAT"s), 3U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance1("XMJYAUZ"s, "MZJAWXU"s), 6U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance1("___XMJYAUZ___"s, "__MZJAWXU___"s),
+                7U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance1("__XMJYAUZ___"s, "___MZJAWXU___-"s),
+                7U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance1("0123456789"s, "0123456789"s), 0U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance1("01234567"s, "abcdefghij"s), 10U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance1("aaabbb"s, "aaabbb___bbbccc"s), 9U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance1("bbbccc"s, "aaabbb___bbbccc"s), 9U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance1("aaabbbccc"s, "aaabbb___bbbccc"s), 6U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance1("sitting"s, "kitten"s), 3U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance1("Sunday"s, "Saturday"s), 3U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance1("CA"s, "ABC"s), 2U);
 }
 
 TLO_TEST(damerLevenDistance2_with_damerLevenDistance1) {
-  TLO_EXPECT(tlo::damerLevenDistance2(""s, ""s) == 0U);
-  TLO_EXPECT(tlo::damerLevenDistance2("GAC"s, ""s) == 3U);
-  TLO_EXPECT(tlo::damerLevenDistance2(""s, "AGCAT"s) == 5U);
-  TLO_EXPECT(tlo::damerLevenDistance2("GAC"s, "AGCAT"s) == 3U);
-  TLO_EXPECT(tlo::damerLevenDistance2("XMJYAUZ"s, "MZJAWXU"s) == 6U);
-  TLO_EXPECT(tlo::damerLevenDistance2("___XMJYAUZ___"s, "__MZJAWXU___"s) == 7U);
-  TLO_EXPECT(tlo::damerLevenDistance2("__XMJYAUZ___"s, "___MZJAWXU___-"s) ==
-             7U);
-  TLO_EXPECT(tlo::damerLevenDistance2("0123456789"s, "0123456789"s) == 0U);
-  TLO_EXPECT(tlo::damerLevenDistance2("01234567"s, "abcdefghij"s) == 10U);
-  TLO_EXPECT(tlo::damerLevenDistance2("aaabbb"s, "aaabbb___bbbccc"s) == 9U);
-  TLO_EXPECT(tlo::damerLevenDistance2("bbbccc"s, "aaabbb___bbbccc"s) == 9U);
-  TLO_EXPECT(tlo::damerLevenDistance2("aaabbbccc"s, "aaabbb___bbbccc"s) == 6U);
-  TLO_EXPECT(tlo::damerLevenDistance2("sitting"s, "kitten"s) == 3U);
-  TLO_EXPECT(tlo::damerLevenDistance2("Sunday"s, "Saturday"s) == 3U);
-  TLO_EXPECT(tlo::damerLevenDistance2("CA"s, "ABC"s) == 2U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance2(""s, ""s), 0U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance2("GAC"s, ""s), 3U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance2(""s, "AGCAT"s), 5U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance2("GAC"s, "AGCAT"s), 3U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance2("XMJYAUZ"s, "MZJAWXU"s), 6U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance2("___XMJYAUZ___"s, "__MZJAWXU___"s),
+                7U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance2("__XMJYAUZ___"s, "___MZJAWXU___-"s),
+                7U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance2("0123456789"s, "0123456789"s), 0U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance2("01234567"s, "abcdefghij"s), 10U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance2("aaabbb"s, "aaabbb___bbbccc"s), 9U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance2("bbbccc"s, "aaabbb___bbbccc"s), 9U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance2("aaabbbccc"s, "aaabbb___bbbccc"s), 6U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance2("sitting"s, "kitten"s), 3U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance2("Sunday"s, "Saturday"s), 3U);
+  TLO_EXPECT_EQ(tlo::damerLevenDistance2("CA"s, "ABC"s), 2U);
 }
 }  // namespace
--- test/lcs-test.cpp
+++ test/lcs-test.cpp
@@ -3,94 +3,99 @@
 #include <tlo-cpp/test.hpp>
 
 namespace {
-template <class T>
-bool equal(const T &obj1, const T &obj2) {
-  return obj1 == obj2;
-}
-
 using namespace std::string_literals;
 
+tlo::LcsLengthResult result(std::size_t lcsLength, std::size_t lcsDistance) {
+  return {lcsLength, lcsDistance};
+}
+
 TLO_TEST(lcsLength1) {
-  TLO_EXPECT(equal(tlo::lcsLength1(""s, ""s), {0, 0}));
-  TLO_EXPECT(equal(tlo::lcsLength1("GAC"s, ""s), {0, 3}));
-  TLO_EXPECT(equal(tlo::lcsLength1(""s, "AGCAT"s), {0, 5}));
-  TLO_EXPECT(equal(tlo::lcsLength1("GAC"s, "AGCAT"s), {2, 4}));
-  TLO_EXPECT(equal(tlo::lcsLength1("XMJYAUZ"s, "MZJAWXU"s), {4, 6}));
-  TLO_EXPECT(equal(tlo::lcsLength1("___XMJYAUZ___"s, "__MZJAWXU___"s), {9, 7}));
-  TLO_EXPECT(
-      equal(tlo::lcsLength1("__XMJYAUZ___"s, "___MZJAWXU___-"s), {9, 8}));
-  TLO_EXPECT(equal(tlo::lcsLength1("0123456789"s, "0123456789"s), {10, 0}));
-  TLO_EXPECT(equal(tlo::lcsLength1("01234567"s, "abcdefghij"s), {0, 18}));
-  TLO_EXPECT(equal(tlo::lcsLength1("aaabbb"s, "aaabbb___bbbccc"s), {6, 9}));
-  TLO_EXPECT(equal(tlo::lcsLength1("bbbccc"s, "aaabbb___bbbccc"s), {6, 9}));
-  TLO_EXPECT(equal(tlo::lcsLength1("aaabbbccc"s, "aaabbb___bbbccc"s), {9, 6}));
-  TLO_EXPECT(equal(tlo::lcsLength1("sitting"s, "kitten"s), {4, 5}));
-  TLO_EXPECT(equal(tlo::lcsLength1("Sunday"s, "Saturday"s), {5, 4}));
-  TLO_EXPECT(equal(tlo::lcsLength1("CA"s, "ABC"s), {1, 3}));
+  TLO_EXPECT_EQ(tlo::lcsLength1(""s, ""s), result(0, 0));
+  TLO_EXPECT_EQ(tlo::lcsLength1("GAC"s, ""s), result(0, 3));
+  TLO_EXPECT_EQ(tlo::lcsLength1(""s, "AGCAT"s), result(0, 5));
+  TLO_EXPECT_EQ(tlo::lcsLength1("GAC"s, "AGCAT"s), result(2, 4));
+  TLO_EXPECT_EQ(tlo::lcsLength1("XMJYAUZ"s, "MZJAWXU"s), result(4, 6));
+  TLO_EXPECT_EQ(tlo::lcsLength1("___XMJYAUZ___"s, "__MZJAWXU___"s),
+                result(9, 7));
+  TLO_EXPECT_EQ(tlo::lcsLength1("__XMJYAUZ___"s, "___MZJAWXU___-"s),
+                result(9, 8));
+  TLO_EXPECT_EQ(tlo::lcsLength1("0123456789"s, "0123456789"s), result(10, 0));
+  TLO_EXPECT_EQ(tlo::lcsLength1("01234567"s, "abcdefghij"s), result(0, 18));
+  TLO_EXPECT_EQ(tlo::lcsLength1("aaabbb"s, "aaabbb___bbbccc"s), result(6, 9));
+  TLO_EXPECT_EQ(tlo::lcsLength1("bbbccc"s, "aaabbb___bbbccc"s), result(6, 9));
+  TLO_EXPECT_EQ(tlo::lcsLength1("aaabbbccc"s, "aaabbb___bbbccc"s),
+                result(9, 6));
+  TLO_EXPECT_EQ(tlo::lcsLength1("sitting"s, "kitten"s), result(4, 5));
+  TLO_EXPECT_EQ(tlo::lcsLength1("Sunday"s, "Saturday"s), result(5, 4));
+  TLO_EXPECT_EQ(tlo::lcsLength1("CA"s, "ABC"s), result(1, 3));
 }
 
 TLO_TEST(lcsLength2) {
-  TLO_EXPECT(equal(tlo::lcsLength2(""s, ""s), {0, 0}));
-  TLO_EXPECT(equal(tlo::lcsLength2("GAC"s, ""s), {0, 3}));
-  TLO_EXPECT(equal(tlo::lcsLength2(""s, "AGCAT"s), {0, 5}));
-  TLO_EXPECT(equal(tlo::lcsLength2("GAC"s, "AGCAT"s), {2, 4}));
-  TLO_EXPECT(equal(tlo::lcsLength2("XMJYAUZ"s, "MZJAWXU"s), {4, 6}));
-  TLO_EXPECT(equal(tlo::lcsLength2("___XMJYAUZ___"s, "__MZJAWXU___"s), {9, 7}));
-  TLO_EXPECT(
-      equal(tlo::lcsLength2("__XMJYAUZ___"s, "___MZJAWXU___-"s), {9, 8}));
-  TLO_EXPECT(equal(tlo::lcsLength2("0123456789"s, "0123456789"s), {10, 0}));
-  TLO_EXPECT(equal(tlo::lcsLength2("01234567"s, "abcdefghij"s), {0, 18}));
-  TLO_EXPECT(equal(tlo::lcsLength2("aaabbb"s, "aaabbb___bbbccc"s), {6, 9}));
-  TLO_EXPECT(equal(tlo::lcsLength2("bbbccc"s, "aaabbb___bbbccc"s), {6, 9}));
-  TLO_EXPECT(equal(tlo::lcsLength2("aaabbbccc"s, "aaabbb___bbbccc"s), {9, 6}));
-  TLO_EXPECT(equal(tlo::lcsLength2("sitting"s, "kitten"s), {4, 5}));
-  TLO_EXPECT(equal(tlo::lcsLength2("Sunday"s, "Saturday"s), {5, 4}));
-  TLO_EXPECT(equal(tlo::lcsLength2("CA"s, "ABC"s), {1, 3}));
+  TLO_EXPECT_EQ(tlo::lcsLength2(""s, ""s), result(0, 0));
+  TLO_EXPECT_EQ(tlo::lcsLength2("GAC"s, ""s), result(0, 3));
+  TLO_EXPECT_EQ(tlo::lcsLength2(""s, "AGCAT"s), result(0, 5));
+  TLO_EXPECT_EQ(tlo::lcsLength2("GAC"s, "AGCAT"s), result(2, 4));
+  TLO_EXPECT_EQ(tlo::lcsLength2("XMJYAUZ"s, "MZJAWXU"s), result(4, 6));
+  TLO_EXPECT_EQ(tlo::lcsLength2("___XMJYAUZ___"s, "__MZJAWXU___"s),
+                result(9, 7));
+  TLO_EXPECT_EQ(tlo::lcsLength2("__XMJYAUZ___"s, "___MZJAWXU___-"s),
+                result(9, 8));
+  TLO_EXPECT_EQ(tlo::lcsLength2("0123456789"s, "0123456789"s), result(10, 0));
+  TLO_EXPECT_EQ(tlo::lcsLength2("01234567"s, "abcdefghij"s), result(0, 18));
+  TLO_EXPECT_EQ(tlo::lcsLength2("aaabbb"s, "aaabbb___bbbccc"s), result(6, 9));
+  TLO_EXPECT_EQ(tlo::lcsLength2("bbbccc"s, "aaabbb___bbbccc"s), result(6, 9));
+  TLO_EXPECT_EQ(tlo::lcsLength2("aaabbbccc"s, "aaabbb___bbbccc"s),
+                result(9, 6));
+  TLO_EXPECT_EQ(tlo::lcsLength2("sitting"s, "kitten"s), result(4, 5));
+  TLO_EXPECT_EQ(tlo::lcsLength2("Sunday"s, "Saturday"s), result(5, 4));
+  TLO_EXPECT_EQ(tlo::lcsLength2("CA"s, "ABC"s), result(1, 3));
 }
 
 TLO_TEST(lcsLength3_with_lcsLength1) {
   const auto &lcsl1_ = tlo::lcsLength1_<std::string>;
 
-  TLO_EXPECT(equal(tlo::lcsLength3(""s, ""s, lcsl1_), {0, 0}));
-  TLO_EXPECT(equal(tlo::lcsLength3("GAC"s, ""s, lcsl1_), {0, 3}));
-  TLO_EXPECT(equal(tlo::lcsLength3(""s, "AGCAT"s, lcsl1_), {0, 5}));
-  TLO_EXPECT(equal(tlo::lcsLength3("GAC"s, "AGCAT"s, lcsl1_), {2, 4}));
-  TLO_EXPECT(equal(tlo::lcsLength3("XMJYAUZ"s, "MZJAWXU"s, lcsl1_), {4, 6}));
-  TLO_EXPECT(equal(tlo::lcsLength3("___XMJYAUZ___"s, "__MZJAWXU___"s, lcsl1_),
-                   {9, 7}));
-  TLO_EXPECT(equal(tlo::lcsLength3("__XMJYAUZ___"s, "___MZJAWXU___-"s, lcsl1_),
-                   {9, 8}));
-  TLO_EXPECT(
-      equal(tlo::lcsLength3("0123456789"s, "0123456789"s, lcsl1_), {10, 0}));
-  TLO_EXPECT(
-      equal(tlo::lcsLength3("01234567"s, "abcdefghij"s, lcsl1_), {0, 18}));
-  TLO_EXPECT(
-      equal(tlo::lcsLength3("aaabbb"s, "aaabbb___bbbccc"s, lcsl1_), {6, 9}));
-  TLO_EXPECT(
-      equal(tlo::lcsLength3("bbbccc"s, "aaabbb___bbbccc"s, lcsl1_), {6, 9}));
-  TLO_EXPECT(
-      equal(tlo::lcsLength3("aaabbbccc"s, "aaabbb___bbbccc"s, lcsl1_), {9, 6}));
-  TLO_EXPECT(equal(tlo::lcsLength3("sitting"s, "kitten"s, lcsl1_), {4, 5}));
-  TLO_EXPECT(equal(tlo::lcsLength3("Sunday"s, "Saturday"s, lcsl1_), {5, 4}));
-  TLO_EXPECT(equal(tlo::lcsLength3("CA"s, "ABC"s, lcsl1_), {1, 3}));
+  TLO_EXPECT_EQ(tlo::lcsLength3(""s, ""s, lcsl1_), result(0, 0));
+  TLO_EXPECT_EQ(tlo::lcsLength3("GAC"s, ""s, lcsl1_), result(0, 3));
+  TLO_EXPECT_EQ(tlo::lcsLength3(""s, "AGCAT"s, lcsl1_), result(0, 5));
+  TLO_EXPECT_EQ(tlo::lcsLength3("GAC"s, "AGCAT"s, lcsl1_), result(2, 4));
+  TLO_EXPECT_EQ(tlo::lcsLength3("XMJYAUZ"s, "MZJAWXU"s, lcsl1_), result(4, 6));
+  TLO_EXPECT_EQ(tlo::lcsLength3("___XMJYAUZ___"s, "__MZJAWXU___"s, lcsl1_),
+                result(9, 7));
+  TLO_EXPECT_EQ(tlo::lcsLength3("__XMJYAUZ___"s, "___MZJAWXU___-"s, lcsl1_),
+                result(9, 8));
+  TLO_EXPECT_EQ(tlo::lcsLength3("0123456789"s, "0123456789"s, lcsl1_),
+                result(10, 0));
+  TLO_EXPECT_EQ(tlo::lcsLength3("01234567"s, "abcdefghij"s, lcsl1_),
+                result(0, 18));
+  TLO_EXPECT_EQ(tlo::lcsLength3("aaabbb"s, "aaabbb___bbbccc"s, lcsl1_),
+                result(6, 9));
+  TLO_EXPECT_EQ(tlo::lcsLength3("bbbccc"s, "aaabbb___bbbccc"s, lcsl1_),
+                result(6, 9));
+  TLO_EXPECT_EQ(tlo::lcsLength3("aaabbbccc"s, "aaabbb___bbbccc"s, lcsl1_),
+                result(9, 6));
+  TLO_EXPECT_EQ(tlo::lcsLength3("sitting"s, "kitten"s, lcsl1_), result(4, 5));
+  TLO_EXPECT_EQ(tlo::lcsLength3("Sunday"s, "Saturday"s, lcsl1_), result(5, 4));
+  TLO_EXPECT_EQ(tlo::lcsLength3("CA"s, "ABC"s, lcsl1_), result(1, 3));
 }
 
 TLO_TEST(lcsLength3_with_lcsLength2) {
-  TLO_EXPECT(equal(tlo::lcsLength3(""s, ""s), {0, 0}));
-  TLO_EXPECT(equal(tlo::lcsLength3("GAC"s, ""s), {0, 3}));
-  TLO_EXPECT(equal(tlo::lcsLength3(""s, "AGCAT"s), {0, 5}));
-  TLO_EXPECT(equal(tlo::lcsLength3("GAC"s, "AGCAT"s), {2, 4}));
-  TLO_EXPECT(equal(tlo::lcsLength3("XMJYAUZ"s, "MZJAWXU"s), {4, 6}));
-  TLO_EXPECT(equal(tlo::lcsLength3("___XMJYAUZ___"s, "__MZJAWXU___"s), {9, 7}));
-  TLO_EXPECT(
-      equal(tlo::lcsLength3("__XMJYAUZ___"s, "___MZJAWXU___-"s), {9, 8}));
-  TLO_EXPECT(equal(tlo::lcsLength3("0123456789"s, "0123456789"s), {10, 0}));
-  TLO_EXPECT(equal(tlo::lcsLength3("01234567"s, "abcdefghij"s), {0, 18}));
-  TLO_EXPECT(equal(tlo::lcsLength3("aaabbb"s, "aaabbb___bbbccc"s), {6, 9}));
-  TLO_EXPECT(equal(tlo::lcsLength3("bbbccc"s, "aaabbb___bbbccc"s), {6, 9}));
-  TLO_EXPECT(equal(tlo::lcsLength3("aaabbbccc"s, "aaabbb___bbbccc"s), {9, 6}));
-  TLO_EXPECT(equal(tlo::lcsLength3("sitting"s, "kitten"s), {4, 5}));
-  TLO_EXPECT(equal(tlo::lcsLength3("Sunday"s, "Saturday"s), {5, 4}));
-  TLO_EXPECT(equal(tlo::lcsLength3("CA"s, "ABC"s), {1, 3}));
+  TLO_EXPECT_EQ(tlo::lcsLength3(""s, ""s), result(0, 0));
+  TLO_EXPECT_EQ(tlo::lcsLength3("GAC"s, ""s), result(0, 3));
+  TLO_EXPECT_EQ(tlo::lcsLength3(""s, "AGCAT"s), result(0, 5));
+  TLO_EXPECT_EQ(tlo::lcsLength3("GAC"s, "AGCAT"s), result(2, 4));
+  TLO_EXPECT_EQ(tlo::lcsLength3("XMJYAUZ"s, "MZJAWXU"s), result(4, 6));
+  TLO_EXPECT_EQ(tlo::lcsLength3("___XMJYAUZ___"s, "__MZJAWXU___"s),
+                result(9, 7));
+  TLO_EXPECT_EQ(tlo::lcsLength3("__XMJYAUZ___"s, "___MZJAWXU___-"s),
+                result(9, 8));
+  TLO_EXPECT_EQ(tlo::lcsLength3("0123456789"s, "0123456789"s), result(10, 0));
+  TLO_EXPECT_EQ(tlo::lcsLength3("01234567"s, "abcdefghij"s), result(0, 18));
+  TLO_EXPECT_EQ(tlo::lcsLength3("aaabbb"s, "aaabbb___bbbccc"s), result(6, 9));
+  TLO_EXPECT_EQ(tlo::lcsLength3("bbbccc"s, "aaabbb___bbbccc"s), result(6, 9));
+  TLO_EXPECT_EQ(tlo::lcsLength3("aaabbbccc"s, "aaabbb___bbbccc"s),
+                result(9, 6));
+  TLO_EXPECT_EQ(tlo::lcsLength3("sitting"s, "kitten"s), result(4, 5));
+  TLO_EXPECT_EQ(tlo::lcsLength3("Sunday"s, "Saturday"s), result(5, 4));
+  TLO_EXPECT_EQ(tlo::lcsLength3("CA"s, "ABC"s), result(1, 3));
 }
 }  // namespace
